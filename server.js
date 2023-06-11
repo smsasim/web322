cdcad9c37343545d578e6fe11ce3cbca75216a27
smsasim <113373403+smsasim@users.noreply.github.com>
2023-06-12 02:38:40 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+
 /*********************************************************************************
 
 WEB322 – Assignment 02
@@ -19,9 +20,26 @@ GitHub Repository URL: git@github.com:smsasim/web322.git
 const express = require('express');
 const app = express();
 const store_service = require('./store-service.js');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+require('dotenv').config();
+const upload = multer();
+
+
 
 app.use(express.static('public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+
+//cinfiguration for cloudinary
+
+cloudinary.config({ 
+    cloud_name: 'dbive2awg', 
+    api_key: '931147177796676', 
+    api_secret: 'bs45j9ryOD7bTCOUky1SU0t1Sb4' 
+  });
+
 
 // Route to redirect from "/" to "/about"
 app.get('/', (req, res) => {
@@ -42,14 +60,125 @@ app.get('/shop', (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
+
+
+
+
+app.get('/item/:id', (req, res) => {
+    const itemId = req.params.id;
+    store_service.getItemById(itemId)
+      .then((item) => {
+        res.json(item);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(404).json({ error: 'Item not found' });
+      });
+  });
+
+
 app.get('/items', (req, res) => {
-    store_service.getAllItems()
+    const { category, minDate } = req.query;
+  
+    if (category) {
+      store_service.getItemsByCategory(category)
+        .then((filteredItems) => {
+          res.json(filteredItems);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Failed to fetch items' });
+        });
+    } else if (minDate) {
+      store_service.getItemsByMinDate(minDate)
+        .then((filteredItems) => {
+          res.json(filteredItems);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Failed to fetch items' });
+        });
+    } else {
+        store_service.getAllItems()
         .then(items => {
             let itemsString = items.map(item => JSON.stringify(item)).join(', ');
             res.send(itemsString);
         })
         .catch(err => res.status(500).send(err));
-});
+    }
+  });
+
+
+  
+app.get('/items/add', (req, res) => {
+    res.sendFile(__dirname + '/views/add_item.html');
+    });
+
+
+//add items
+
+app.post('/items/add', upload.single('featureImage'), async (req, res) => {
+
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processItem(uploaded.url);
+        });
+    }else{
+        processItem("");
+    }
+     
+    function processItem(imageUrl){
+        req.body.featureImage = imageUrl;
+    
+        const newItem = {
+        
+        title: req.body.title,
+        description: req.body.body,
+        featureImage: req.body.featureImage,
+        category : req.body.category,
+        visibility : req.body.published,
+        
+        };
+
+        store_service.addItem(newItem)
+        .then((addedItem) => {
+            res.redirect('/items');
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to add item' });
+        });
+    } 
+  });
+  
+  
+
+
+
+
+
 
 app.get('/categories', (req, res) => {
     store_service.getCategories()
@@ -65,7 +194,7 @@ app.use('*', (req, res) => {
 });
 
 // Setting up the port for the server to listen on
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 store_service.initialize()
     .then(() => {
--- store-service.js
+++ store-service.js
@@ -51,9 +51,77 @@ function getCategories() {
     });
 }
 
+
+
+function addItem(itemData) {
+    return new Promise((resolve, reject) => {
+      if (itemData.published === undefined) {
+        itemData.published = false;
+      } else {
+        itemData.published = true;
+      }
+  
+      itemData.id = items.length + 1;
+  
+      items.push(itemData);
+      resolve(itemData);
+    });
+  }
+
+
+
+  function getItemsByCategory(category) {
+    return new Promise((resolve, reject) => {
+      const filteredItems = items.filter((item) => item.category === category);
+      if (filteredItems.length > 0) {
+        resolve(filteredItems);
+      } else {
+        reject('No results returned');
+      }
+    });
+  }
+  
+  
+  
+  function getItemsByMinDate(minDateStr) {
+    return new Promise((resolve, reject) => {
+      const filteredItems = items.filter((item) => new Date(item.postDate) >= new Date(minDateStr));
+      if (filteredItems.length > 0) {
+        resolve(filteredItems);
+      } else {
+        reject('No results returned');
+      }
+    });
+  }
+  
+
+
+
+  function getItemById(id) {
+    return new Promise((resolve, reject) => {
+      const item = items.find((item) => item.id === id);
+      if (item) {
+        resolve(item);
+      } else {
+        reject('No result returned');
+      }
+    });
+  }
+  
+  
+  
+
+
+
+
+
 module.exports = {
     initialize,
     getAllItems,
     getPublishedItems,
-    getCategories
+    addItem,
+    getCategories,
+    getItemsByCategory,
+    getItemsByMinDate,
+    getItemById,
 };
